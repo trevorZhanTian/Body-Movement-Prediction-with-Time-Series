138f5791a1afaf8c0a6374c83e28d1e5d0b51938
TrevorZhan <tzhan@brooksschool.org>
2025-12-09 08:46:03 -0500

    tried converting results to df and switched to notebooks

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from ultralytics import YOLO
-import cv2
+import pandas as pd
 
 
 source="squat2.mov"  # Sample image for testing
-
-# ==================================================
-# Pose estimation: includes, class index, bounding
-# box in xyxy, xywh format, keypoints and confidence
-# ==================================================
 model = YOLO("yolo11n-pose.pt")
 class_names = model.names  # Store model classes names
-results = model.predict(source=source, show=True, save=True)
+results = model(source=source, stream=True)
+
+data = []
 
 for result in results:
-    bbox_list = result.boxes.xyxy.tolist()          # bounding boxes all objects, you can also get xywh with boxes.xywh
-    clss_list = result.boxes.cls.int().tolist()     # class index all objects
-    conf_list = result.boxes.conf.tolist()          # confidence list all objects
     kpt_list = result.keypoints.xy                 # keypoints for all objects
-    for box, kpt, cls, conf in zip(bbox_list, kpt_list, clss_list, conf_list):  # bbox, kpt, cls, id & conf
-        print(f"Bounding box: {box}, Class index: {cls}, Class name: {class_names[cls]}, "
-              f"Keypoints data: {kpt}, Confidence: {conf}")
+    for kpt in kpt_list:  
+        for x, y in kpt.tolist(): 
+            data.append({
+                'x': x,
+                'y': y
+            })
+        
+df = pd.DataFrame(data)
+print(df.head(20))
